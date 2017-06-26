eb4b9c2ee78fa953cf2a7989fb8b3d190f4c275d
mk-kimjh <mk.kimjh@gmail.com>
2017-06-26 20:11:45 +0900

    레이아웃 & 메인페이지 작성 중...

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 	<title>우정바이오사업부</title>
 	<link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<link href="/assets/css/default.css" rel="stylesheet">
+	<link href="/assets/css/main.css" rel="stylesheet">
 	<!--[if lt IE 9]>
 	  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -21,7 +22,7 @@
 	<header id="header">
 		<div class="container">
 			<div class="row">
-				<div class="col-xs-8">
+				<div class="col-xs-8 col-sm-4 col-lg-3">
 					<h1 class="logo">
 						<a href="#">
 							<img class="visible-xs" src="/assets/images/logo_xs.png" alt="우정바이오사업본부">
@@ -30,128 +31,188 @@
 						</a>
 					</h1>
 				</div>
-				<div class="col-xs-4">
-					<nav>
+				<div class="col-xs-4 col-sm-2 col-sm-offset-6 text-right hidden-lg">
+					<button id="btn-open-side-nav" class="btn btn-link" type="button">
+						<span class="sr-only">사이드 메뉴 열기</span>
+					</button>
+				</div>
+				<div class="col-lg-9 visible-lg">
+					<nav id="nav">
+						<ul>
+							<li><a href="#">R&amp;D</a></li>
+							<li><a href="#">CRO</a></li>
+							<li><a href="#">실험동물센터</a></li>
+							<li><a href="#">PRODUCT</a></li>
+							<li><a href="#">문의</a></li>
+						</ul>
 					</nav>
 				</div>
 			</div>
 		</div>
 	</header>
-	<div id="fields">
-		<div class="container-fluid">
-			<div class="row">
-				<div class="col-xs-6 col-sm-3">
-					<div class="field-item field-item-1" tabindex="0">
-						<div class="field-back">
-							<video id="field-video-1" loop muted>
-								<source src="/assets/images/video_1.mp4" type="video/mp4">
-							  	Your browser does not support the video tag.
-							</video>
-						</div>
-						<div class="field-content">
-							<div class="field-body">
-								<h4>ANIMAL CENTER</h4>
-								<hr>
-								<p>(주)우정 바이오사업부의 Products를 <br>소개해드립니다.</p>
-								<ul>
-									<li><a href="#">mAb</a></li>
-									<li><a href="#">Lentivirus</a></li>
-									<li><a href="#">Stable Cell</a></li>
-									<li><a href="#">CDX/PDX</a></li>
-									<li><a href="#">Disease Model</a></li>
-								</ul>
+	<div id="nav-side" class="hidden-lg" role="presentation" style="display: none;">
+		#nav-side
+	</div>
+	
+	<main id="content">
+		<section id="fields">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-xs-6 col-md-3">
+						<div class="field-item field-item-1" tabindex="0">
+							<div class="field-back">
+								<video id="field-video-1" loop muted>
+									<source src="/assets/images/video_1.mp4" type="video/mp4">
+									Your browser does not support the video tag.
+								</video>
+							</div>
+							<div class="field-content">
+								<div class="field-body">
+									<h4><a href="#">ANIMAL CENTER</a></h4>
+									<hr>
+									<p>(주)우정 바이오사업부의 Products를 <br>소개해드립니다.</p>
+									<ul>
+										<li><a href="#">mAb</a></li>
+										<li><a href="#">Lentivirus</a></li>
+										<li><a href="#">Stable Cell</a></li>
+										<li><a href="#">CDX/PDX</a></li>
+										<li><a href="#">Disease Model</a></li>
+									</ul>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-				<div class="col-xs-6 col-sm-3">
-					<div class="field-item field-item-2 active" tabindex="0">
-						<div class="field-back">
-							<video id="field-video-2" loop muted>
-								<source src="/assets/images/video_2.mp4" type="video/mp4">
-							  	Your browser does not support the video tag.
-							</video>
-						</div>
-						<div class="field-content">
-							<div class="field-body">
-								<h4>R&amp;D</h4>
-								<hr>
-								<p>(주)우정 바이오사업부의 Products를 <br>소개해드립니다.</p>
-								<ul>
-									<li><a href="#">mAb</a></li>
-									<li><a href="#">Lentivirus</a></li>
-									<li><a href="#">Stable Cell</a></li>
-									<li><a href="#">CDX/PDX</a></li>
-									<li><a href="#">Disease Model</a></li>
-								</ul>
+					<div class="col-xs-6 col-md-3">
+						<div class="field-item field-item-2 active" tabindex="0">
+							<div class="field-back">
+								<video id="field-video-2" loop muted>
+									<source src="/assets/images/video_2.mp4" type="video/mp4">
+									Your browser does not support the video tag.
+								</video>
+							</div>
+							<div class="field-content">
+								<div class="field-body">
+									<h4><a href="#">R&amp;D</a></h4>
+									<hr>
+									<p>(주)우정 바이오사업부의 Products를 <br>소개해드립니다.</p>
+									<ul>
+										<li><a href="#">mAb</a></li>
+										<li><a href="#">Lentivirus</a></li>
+										<li><a href="#">Stable Cell</a></li>
+										<li><a href="#">CDX/PDX</a></li>
+										<li><a href="#">Disease Model</a></li>
+									</ul>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-				<div class="col-xs-6 col-sm-3">
-					<div class="field-item field-item-3" tabindex="0">
-						<div class="field-back">
-							<video id="field-video-3" loop muted>
-								<source src="/assets/images/video_3.mp4" type="video/mp4">
-							  	Your browser does not support the video tag.
-							</video>
-						</div>
-						<div class="field-content">
-							<div class="field-body">
-								<h4>PRODUCTS</h4>
-								<hr>
-								<p>(주)우정 바이오사업부의 Products를 <br>소개해드립니다.</p>
-								<ul>
-									<li><a href="#">mAb</a></li>
-									<li><a href="#">Lentivirus</a></li>
-									<li><a href="#">Stable Cell</a></li>
-									<li><a href="#">CDX/PDX</a></li>
-									<li><a href="#">Disease Model</a></li>
-								</ul>
+					<div class="col-xs-6 col-md-3">
+						<div class="field-item field-item-3" tabindex="0">
+							<div class="field-back">
+								<video id="field-video-3" loop muted>
+									<source src="/assets/images/video_3.mp4" type="video/mp4">
+									Your browser does not support the video tag.
+								</video>
+							</div>
+							<div class="field-content">
+								<div class="field-body">
+									<h4><a href="#">PRODUCTS</a></h4>
+									<hr>
+									<p>(주)우정 바이오사업부의 Products를 <br>소개해드립니다.</p>
+									<ul>
+										<li><a href="#">mAb</a></li>
+										<li><a href="#">Lentivirus</a></li>
+										<li><a href="#">Stable Cell</a></li>
+										<li><a href="#">CDX/PDX</a></li>
+										<li><a href="#">Disease Model</a></li>
+									</ul>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-				<div class="col-xs-6 col-sm-3">
-					<div class="field-item field-item-4" tabindex="0">
-						<div class="field-back">
-							<video id="field-video-4" loop muted>
-								<source src="/assets/images/video_4.mp4" type="video/mp4">
-							  	Your browser does not support the video tag.
-							</video>
-						</div>
-						<div class="field-content">
-							<div class="field-body">
-								<h4>CRO</h4>
-								<hr>
-								<p>(주)우정 바이오사업부의 Products를 <br>소개해드립니다.</p>
-								<ul>
-									<li><a href="#">mAb</a></li>
-									<li><a href="#">Lentivirus</a></li>
-									<li><a href="#">Stable Cell</a></li>
-									<li><a href="#">CDX/PDX</a></li>
-									<li><a href="#">Disease Model</a></li>
-								</ul>
+					<div class="col-xs-6 col-md-3">
+						<div class="field-item field-item-4" tabindex="0">
+							<div class="field-back">
+								<video id="field-video-4" loop muted>
+									<source src="/assets/images/video_4.mp4" type="video/mp4">
+									Your browser does not support the video tag.
+								</video>
+							</div>
+							<div class="field-content">
+								<div class="field-body">
+									<h4><a href="#">CRO</a></h4>
+									<hr>
+									<p>(주)우정 바이오사업부의 Products를 <br>소개해드립니다.</p>
+									<ul>
+										<li><a href="#">mAb</a></li>
+										<li><a href="#">Lentivirus</a></li>
+										<li><a href="#">Stable Cell</a></li>
+										<li><a href="#">CDX/PDX</a></li>
+										<li><a href="#">Disease Model</a></li>
+									</ul>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	</div>
-	<main id="content">
-		<div class="container">
-			<p><img src="/assets/images/img_content.jpg" alt=""></p>
-		</div>
+		</section>
 	</main>
 	<footer id="footer">
 		<div class="container">
-			<p><img src="/assets/images/img_footer.jpg" alt=""></p>
+			<div class="row">
+				<div class="col-xs-12 col-sm-9 col-lg-10">
+					<div class="address">
+						<dl>
+							<dt>주소</dt>
+							<dd><address>경기도 수원시 영통구 광교로 145 차세대융합기술연구원 B동 3층 (주)우정비에스씨</address></dd>
+						</dl>
+					</div>
+					<div class="contact">
+						<dl>
+							<dt>TEL</dt>
+							<dd>031-888-9369</dd>
+						</dl>
+						<dl>
+							<dt>FAX</dt>
+							<dd>031-888-9368</dd>
+						</dl>
+						<dl>
+							<dt>E-mail</dt>
+							<dd><a href="mailto:wj@woojungbsc.co.kr" target="_blank">wj@woojungbsc.co.kr</a></dd>
+						</dl>
+					</div>
+					<div class="copyright">
+						<p>
+							COPYRIGHT (C) 2017 WOOJUNGBSC.,CO.LTD.<br class="visible-xs">
+							ALL RIGHTS RESERVED.<br class="hidden-lg">
+							SITE BY <a href="http://movementk.com" target="_blank">MOVEMENT K GROUP</a>.
+						</p>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-3 col-lg-2">
+					<div class="family">
+						<div class="dropup">
+							<button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">
+								FAMILY SITE
+								<span class="caret"></span>
+							</button>
+							<ul class="dropdown-menu">
+								<li><a href="#">FAMILY SITE #1</a></li>
+								<li><a href="#">FAMILY SITE #2</a></li>
+								<li><a href="#">FAMILY SITE #3</a></li>
+								<li><a href="#">FAMILY SITE #4</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</footer>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	<script src="/assets/bootstrap/js/bootstrap.min.js"></script>
 	<script>
+		/*
 		(function($) {
 			$(document).on("mouseenter focusin", "#fields .field-item", function() {
 				var fieldVideo = document.getElementById("field-video-"+($(this).parent().index()+1));
@@ -166,6 +227,7 @@
 				}
 			});
 		})(jQuery);
+		*/
 	</script>
 </body>
 </html>
