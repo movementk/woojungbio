10ebf47dbb73d108f7a3d182d2238c3f253ad113
mk-kimjh <mk.kimjh@gmail.com>
2017-07-05 18:56:40 +0900

    메인 페이지 작성 중...(중간보고 작업본)

---
+++ inc/top-nav.php
@@ -0,0 +1,13 @@
+<div id="top-nav" class="visible-lg">
+	<div class="container">
+		<div class="affiliates">
+			<ul>
+				<li><a href="http://www.woojungbsc.co.kr" target="_blank">우정BSC</a></li>
+				<li><a href="#">우정E&amp;C</a></li>
+				<li><a href="#">INNOSERVICE</a></li>
+				<li><a href="http://www.biosafemedicare.co.kr" target="_blank">BSMS</a></li>
+				<li><a class="active" href="#">바이오사업부</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
