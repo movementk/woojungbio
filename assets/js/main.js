9a3c76a1efc0f9105d4dff3ca0da4551c9d8c030
mk-kimjh <mk.kimjh@gmail.com>
2017-07-07 15:26:33 +0900

    메인 작성 완료

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,23 @@
 (function($) {
+	
+	// 인트로 관련
+	$(window).on("load", function() {
+		$("#intro").addClass("on");
+		var introVideo = $("#intro").find("video").show().get(0);
+		if (introVideo) {
+			introVideo.play();
+		}
+	});
+
+	// 스크롤 클래스 설정
+	$(window).on("scroll resize", function() {
+		if ( $("#intro").height() < $(this).scrollTop() ) {
+			$("body").addClass("scrolled");
+		} else {
+			$("body").removeClass("scrolled");
+		}
+	});
+
 	// 사업분야 동영상
 	$(document).on("mouseenter focusin", "#fields .field-item", function() {
 		var fieldVideo = $(this).find("video").get(0);
@@ -12,4 +31,5 @@
 			fieldVideo.pause();
 		}
 	});
+	
 })(jQuery);
--- index.php
+++ index.php
@@ -2,22 +2,21 @@
 <link href="/assets/css/main.css" rel="stylesheet">
 </body>
 <div id="intro">
-	<div class="intro-back">
-		<video loop muted autoplay>
-			<source src="/assets/images/video_intro.mp4" type="video/mp4">
-			Your browser does not support the video tag.
-		</video>
-	</div>
+	<video class="intro-video" loop muted autoplay>
+		<source src="/assets/images/main/video_jumbo.mp4" type="video/mp4">
+		Your browser does not support the video tag.
+	</video>
+	<div class="intro-back"></div>
 	<div class="intro-content">
 		<div>
-			<h1><img src="/assets/images/logo.png" alt="바이오사업본부"></h1>
+			<h1><img src="/assets/images/logo_white.png" alt="바이오사업본부"></h1>
 			<p>
 				우정비에스씨는<br>
 				최첨단 동물실을 기반으로<br>
-				<b>4차 산업의 중심의 정밀의학 분야를<br>
+				<b>4차 산업 중심의 정밀의학 분야를<br>
 				선도하는 기업입니다.<br></b>
 			</p>
-			<i class="mousewheel">
+			<i class="ico-mouse">
 				SCROLL TO<br>MOUSE WHEEL
 			</i>
 		</div>
@@ -38,22 +37,19 @@
 					<div class="field-item field-item-1" tabindex="0">
 						<div class="field-back">
 							<video loop muted>
-								<source src="/assets/images/video_1.mp4" type="video/mp4">
-								Your browser does not support the video tag.
+								<source src="/assets/images/main/video_1.mp4" type="video/mp4">
+								<img src="/assets/images/main/bg_field_1.jpg">
 							</video>
 						</div>
 						<div class="field-content">
 							<div class="field-body">
 								<h4><a href="#">ANIMAL CENTER</a></h4>
 								<hr>
-								<p>
-									(주)우정 바이오사업부의 <br>ANIMAL CENTER를 소개해 드립니다.
-								</p>
 								<ul>
-									<li><a href="#">GEM</a></li>
-									<li><a href="#">IVF</a></li>
-									<li><a href="#">Cleaning</a></li>
-									<li><a href="#">Colony Expansion</a></li>
+									<li><a href="#">실험동물생산</a></li>
+									<li><a href="#">질환모델 제작<br>(KO제작, 관리)</a></li>
+									<li><a href="#">질환모델 유지관리</a></li>
+									<li><a href="#">유전자변형 문의</a></li>
 								</ul>
 							</div>
 						</div>
@@ -63,22 +59,21 @@
 					<div class="field-item field-item-2 active" tabindex="0">
 						<div class="field-back">
 							<video loop muted>
-								<source src="/assets/images/video_2.mp4" type="video/mp4">
-								Your browser does not support the video tag.
+								<source src="/assets/images/main/video_2.mp4" type="video/mp4">
+								<img src="/assets/images/main/bg_field_2.jpg">
 							</video>
 						</div>
 						<div class="field-content">
 							<div class="field-body">
-								<h4><a href="#">R&amp;D</a></h4>
+								<h4><a href="#">GLP</a></h4>
 								<hr>
-								<p>
-									(주)우정 바이오사업부의 <br>R&amp;D를 소개해 드립니다.
-								</p>
 								<ul>
-									<li><a href="#">Immune-tech</a></li>
-									<li><a href="#">Advanced</a></li>
-									<li><a href="#">Material</a></li>
-									<li><a href="#">Translation division</a></li>
+									<li><a href="#">일반독성</a></li>
+									<li><a href="#">유전독성</a></li>
+									<li><a href="#">생식발생독성</a></li>
+									<li><a href="#">항원성</a></li>
+									<li><a href="#">수생생태독성</a></li>
+									<li><a href="#">대체독성</a></li>
 								</ul>
 							</div>
 						</div>
@@ -88,23 +83,21 @@
 					<div class="field-item field-item-3" tabindex="0">
 						<div class="field-back">
 							<video loop muted>
-								<source src="/assets/images/video_3.mp4" type="video/mp4">
-								Your browser does not support the video tag.
+								<source src="/assets/images/main/video_3.mp4" type="video/mp4">
+								<img src="/assets/images/main/bg_field_3.jpg">
 							</video>
 						</div>
 						<div class="field-content">
 							<div class="field-body">
 								<h4><a href="#">PRODUCTS</a></h4>
 								<hr>
-								<p>
-									(주)우정 바이오사업부의 <br>PRODUCTS를 소개해 드립니다.
-								</p>
 								<ul>
-									<li><a href="#">mAb</a></li>
-									<li><a href="#">Lentivirus</a></li>
-									<li><a href="#">Stable Cell</a></li>
-									<li><a href="#">CDX/PDX</a></li>
-									<li><a href="#">Disease Model</a></li>
+									<li><a href="#">Lentivirus / 형광세포주</a></li>
+									<li><a href="#">Antibody Production</a></li>
+									<li><a href="#">면역부전 모델 (NPG)</a></li>
+									<li><a href="#">인간화 간 모델 (URG)</a></li>
+									<li><a href="#">바이러스, 백신 연구용 동물 <br>(AG129 / A129)</a></li>
+									<li><a href="#">엘레가드 괴팅겐 미니피그</a></li>
 								</ul>
 							</div>
 						</div>
@@ -114,23 +107,19 @@
 					<div class="field-item field-item-4" tabindex="0">
 						<div class="field-back">
 							<video loop muted>
-								<source src="/assets/images/video_4.mp4" type="video/mp4">
-								Your browser does not support the video tag.
+								<source src="/assets/images/main/video_4.mp4" type="video/mp4">
+								<img src="/assets/images/main/bg_field_4.jpg">
 							</video>
 						</div>
 						<div class="field-content">
 							<div class="field-body">
 								<h4><a href="#">CRO</a></h4>
 								<hr>
-								<p>
-									(주)우정 바이오사업부의 <br>CRO를 소개해 드립니다.
-								</p>
 								<ul>
-									<li><a href="#">GLP</a></li>
-									<li><a href="#">Zebrafish</a></li>
-									<li><a href="#">Precision</a></li>
-									<li><a href="#">Medicine</a></li>
-									<li><a href="#">Global CRO</a></li>
+									<li><a href="#">효능평가 서비스 <br>(대사질환 / 면역종양)</a></li>
+									<li><a href="#">SNBL</a></li>
+									<li><a href="#">Senzagen</a></li>
+									<li><a href="#">Zebrafish Screening</a></li>
 								</ul>
 							</div>
 						</div>
@@ -148,19 +137,6 @@
 <script src="/assets/jquery.bxslider/jquery.bxslider.min.js"></script>
 <script src="/assets/js/common.js"></script>
 <script src="/assets/js/main.js"></script>
-<script>
-	(function($) {
-		$(window).on("scroll", function() {
-			console.log( $("#intro").height() );
-			console.log($(this).scrollTop());
-			if ( $("#intro").height() < $(this).scrollTop() ) {
-				$("body").addClass("scrolled");
-			} else {
-				$("body").removeClass("scrolled");
-			}
-		});
-	})(jQuery);
-</script>
 
 </body>
 </html>
