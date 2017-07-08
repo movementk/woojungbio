d8a79c8b29a71a698e8cfaf621834cc1fb5413a5
mk-kimjh <mk.kimjh@gmail.com>
2017-07-08 11:27:31 +0900

    서브 가이드 작성 중...

--- sub.php
+++ sub.php
@@ -12,8 +12,35 @@
 <main id="content">
 	
 	<div class="page-header">
-		<h1>질환동물 유지관리</h1>
+		<div class="container">
+			<h1>Development</h1>
+			<p>글로벌 메디컬 &amp; 헬스케어 전문기업 <br class="visible-xs">(주)우정비에스씨, <br class="visible-xs visible-sm">도약을 위한 개발은 새로운 도전입니다.</p>
+		</div>
 	</div>
+		
+	<div class="page-summary">
+		<div class="container">
+			<h2>Natural <i>Product</i> Material</h2>
+			<hr>
+			<p>
+				천연물이란 육상 및 해양에 존재하는 동,식물 등의 생물과 생물의 세포 또는 조직배양 산물 등 생물을 기원으로 하는 산물을 말합니다.<br class="hidden-xs">
+				이러한 천연물 유래 소재에 대한 연구는 화학, 천연물약품, 식품, 생물학 분야의 복합적인 지식이 필요로 하는 분야로<br class="hidden-xs">
+				우정비에스씨는 감염병예방, 헬스케어, 바이오 기능소재 연구를 진행하고 있습니다.<br class="hidden-xs">
+				또한 천연물 유래 소재를 통한 제품 연구개발을 위해 다양한 분야의 국내 연구기관 및 대학교와의 협력을 통해 적극적인 연구를 진행하고 있습니다.
+			</p>
+		</div>
+	</div>
+		
+	<section class="section-content">
+		<div class="container">
+			<div class="section-header">
+				<h4 class="section-title">제품화 연구</h4>
+			</div>
+			<div class="section-body">
+				
+			</div>
+		</div>
+	</section>
 			
 </main>
 
