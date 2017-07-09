966bb8b17df98aaa166c99cb99a71a292c9fa484
mk-kimjh <mk.kimjh@gmail.com>
2017-07-09 11:55:43 +0900

    breadcrumbs 추가(폰텔로 추가)

--- sub.php
+++ sub.php
@@ -15,6 +15,17 @@
 	</div>
 </div>
 
+<div class="page-path visible-lg">
+	<div class="container">
+		<ol class="breadcrumb">
+			<li><a href="#"><i class="icon-home"></i></a></li>
+		  	<li><a href="#">CRO</a></li>
+		  	<li><a href="#">정밀의학</a></li>
+		  	<li>기능성 효능평가 서비스</li>
+		</ol>
+	</div>
+</div>
+
 <main id="content">
 	
 	<div class="page-header">
