e2cfe87f4381037f33e66afcc0e8fd96da78ecf1
mk-kimjh <mk.kimjh@gmail.com>
2017-07-09 23:32:51 +0900

    rnd 완료

--- rnd/development.php
+++ rnd/development.php
@@ -20,14 +20,52 @@
 			</p>
 		</div>
 	</div>
-	
+	<div class="page-summary">
+		<div class="container">
+			<p>
+				<b>Development, 우정의 미래입니다.</b><br>
+				우정비에스씨는 글로벌 메디컬 &amp; 헬스케어 전문기업으로 성장 이라는 비젼을 가지고  정밀의약, 글로벌 신약, 감염병 예방용 의약품 , 
+				천연물 신소재 제품화를  하며 미래를 위해 새로운 도전을 지속적으로 추진하고 있습니다.
+			</p>
+			<p>
+				<b>핵심 연구 역량 향상을 통해 세계로 나아가겠습니다.</b><br>
+				지속적인 R&amp;D 투자를 통해  기반 기술 확보를 하며, 차별화된 기술도입을 위해 국내외  연구진들과의 공동연구 체제를 
+				구축하고 있습니다.  또한, 세계적인 연구기관들과의 전략적 제휴를 통해  글로벌 제품 개발을 추진 하고 있습니다.
+			</p>
+		</div>
+	</div>
 	<section class="section-content pipline has-space">
 		<div class="container">
 			<div class="section-header">
 				<h4 class="section-title">개발 제품의 PIPLINE</h4>
 			</div>
 			<div class="section-body">
-				
+				<ul class="pipline-list row">
+					<li class="col-xs-12 col-sm-3 pipline-item pipline-item-1">
+						<figure>
+							<img class="img-responsive" src="/assets/images/icons/rnd/b1.png">
+							<figcaption>1) 탐색 연구</figcaption>
+						</figure>
+					</li>
+					<li class="col-xs-12 col-sm-3 pipline-item pipline-item-2">
+						<figure>
+							<img class="img-responsive" src="/assets/images/icons/rnd/b2.png">
+							<figcaption>2) 연구 단계</figcaption>
+						</figure>
+					</li>
+					<li class="col-xs-12 col-sm-3 pipline-item pipline-item-3">
+						<figure>
+							<img class="img-responsive" src="/assets/images/icons/rnd/b3.png">
+							<figcaption>3) 검증</figcaption>
+						</figure>
+					</li>
+					<li class="col-xs-12 col-sm-3 pipline-item pipline-item-4">
+						<figure>
+							<img class="img-responsive" src="/assets/images/icons/rnd/b4.png">
+							<figcaption>4) 개발</figcaption>
+						</figure>
+					</li>
+				</ul>
 			</div>
 		</div>
 	</section>
@@ -155,6 +193,61 @@
 				<p class="sub-summary">
 				    미래 시장의 성장 가능성이 높은 기술개발을 하기 위해서는  전체적인 기술 동향 파악과 프로젝트 방향 설정이 중요합니다.  
 				</p>
+			</div>
+			<div class="section-body">
+				<div class="row">
+					<ul class="strategy-list">
+						<li class="col-xs-12 col-md-4">
+							<div class="strategy-item">
+								<div class="figure"></div>
+								<div class="details">
+									<h5>제품화 개발 활성화</h5>
+									<hr>
+									<p>
+										이에 당사는 프로젝트 발굴 및 선정을 위해 
+										지속적인 검토회의와 연구 로드맵을 도출하여  
+										중장기 전략과  단기적인 제품 개발 연구를 
+										시작으로 대형 프로젝트 중심의 
+										제품화 개발을 활성화할 계획입니다.
+									</p>
+								</div>
+							</div>
+						</li>
+						<li class="col-xs-12 col-md-4">
+							<div class="strategy-item">
+								<div class="figure"></div>
+								<div class="details">
+									<h5>연구 효율성 극대화</h5>
+									<hr>
+									<p>
+										다양한 기술을 보유하기 위한 내부 역량을 
+										강화하고,  신속한 효능평가 기술을 통해 
+										연구체제를 구축하고 글로벌화가 가능한 기능성 
+										신소재 발굴과 및 연구기획을 통해 
+										외부기관과의 활발한 네트워킹을 통해 연구의 
+										효율성을 극대화할 전략 입니다.
+									</p>
+								</div>
+							</div>
+						</li>
+						<li class="col-xs-12 col-md-4">
+							<div class="strategy-item">
+								<div class="figure"></div>
+								<div class="details">
+									<h5>글로벌 마켓 진출</h5>
+									<hr>
+									<p>
+										우정의 브랜드 제품을 국내시장에 발매하고 
+										지속적인 성장 원동력을 삼기 위하여 국제 수준에 
+										부합하는 제품으로 업그레이드하여 글로벌 마켓 
+										진출을 하고자 하는 중장기 전략 목표를 
+										추진하고자 합니다.
+									</p>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	</section>
