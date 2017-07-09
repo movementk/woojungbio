a2d54e723a9abf1c04d7f7781bb030b72bc00329
kimyh5392 <mk.kimyh@gmail.com>
2017-07-09 14:30:26 +0900

    모든 페이지 생성 및 텍스트 작업 완료.

---
+++ rnd/platform_technology.php
@@ -0,0 +1,166 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link href="/assets/css/sub.css" rel="stylesheet">
+<link href="/assets/css/rnd.css" rel="stylesheet">
+</head>
+<body class="rnd platform-technology">
+
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/top-nav.php'); ?>
+
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/side-nav.php'); ?>
+
+<main id="content">
+	
+    <div class="page-header">
+        <div class="container">
+            <h1>플랫폼 기술</h1>
+        </div>
+    </div>
+	
+    <div class="tabs">
+        <div class="container">
+            <div class="dropdown">
+                <button type="button" class="btn btn-block" data-toggle="dropdown">
+                    <span class="text">Innovation Gene Therapy</span>
+                    <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu" role="tablist">
+                    <li role="presentation" class="active">
+                        <a href="#tab-1" role="tab" data-toggle="tab">Innovation Gene Therapy</a>
+                    </li>
+                    <li role="presentation">
+                        <a href="#tab-2" role="tab" data-toggle="tab">Homologous Epitopes Region Immunization Technology</a>
+                    </li>
+                    <li role="presentation">
+                        <a href="#tab-3" role="tab" data-toggle="tab">Cancer Therapies in Models</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="tab-content">
+        <!-- #tab-1 -->
+        <div role="tabpanel" class="tab-pane active" id="tab-1">
+            <div class="page-summary">
+                <h2>Innovation Gene Therapy</h2>
+                <hr>
+                <p>
+                    높은 역가의 형질도입 가능한 Pseudoviral particle 생성하며,<br>
+                    분열과 비분열 등 광명범위한 세포에 유전자를 효율적으로 전달하고 안정적으로 발현하는 바이러스성 유전자 전달 시스템입니다.<br>
+                    Functional Titer : Flu-(GFP, RFP, mCherry....) tagging 을 사용하여 Pesudoviral titer와 transduction efficiency를 측정합니다.
+                </p>
+            </div>
+            <section class="section-content virus-vector">
+                <div class="container">
+                    <div class="section-header">
+                        <h4 class="section-title">안정성 높은 바이러스성 벡터 개발</h4>
+                    </div>
+                    <div class="section-body">
+                    </div>
+                </div>
+            </section>
+            <section class="section-content stable-development has-space">
+                <div class="container">
+                    <div class="section-header">
+                        <h4 class="section-title">Stable Cell Line Development</h4>
+                    </div>
+                    <div class="section-body">
+                    </div>
+                </div>
+            </section>
+        </div>
+        <!-- #tab-2 -->
+        <div role="tabpanel" class="tab-pane" id="tab-2">
+            <div class="page-summary">
+                <h2>Homologous Epitopes Region Technology</h2>
+                <hr>
+                <p>
+                    우정비에스씨는 핵심 플랫폼인 Homologous Epitopes Region (HERO) method를 바탕으로<br>
+                    기존 단백질에 대한 High-affinity, High-specificity 면역 항체신약을 개발
+                </p>
+            </div>
+            <section class="section-content immune-antibody has-space">
+                <div class="container">
+                    <div class="section-header">
+                        <h4 class="section-title">면역항체</h4>
+                    </div>
+                    <div class="section-body">
+                    </div>
+                </div>
+            </section>
+            <section class="section-content pipeline">
+                <div class="container">
+                    <div class="section-header">
+                        <h4 class="section-title">면역항체 신약 파이프라인</h4>
+                    </div>
+                    <div class="section-body">
+                    </div>
+                </div>
+            </section>
+        </div>
+        <!-- #tab-3 -->
+        <div role="tabpanel" class="tab-pane" id="tab-3">
+            <div class="page-summary">
+                <h2>Cell-line Derived Xenograft Models</h2>
+                <hr>
+                <p>
+                    Cell-line Derived Xenograft Models (이종 이식 모델)은 항 종양 효능 및<br>
+                    치료 기간을 최대화하는 약물 투여의 이상적인 약물 투약, 치료 계획 및 경로를 결정하는 데 일반적으로 사용
+                </p>
+            </div>
+            
+            <section class="section-content xenograft">
+                <div class="container">
+                    <div class="section-header">
+                        <h4 class="section-title">Cell-line Derived Xenograft Product</h4>
+                    </div>
+                    <div class="section-body">
+                        
+                    </div>
+                    <div class="section-header">
+                        <h4 class="section-title">Syngeneic Tumor Models</h4>
+                    </div>
+                    <div class="section-body">
+                        
+                    </div>
+                    <div class="section-header">
+                        <h4 class="section-title">Metastasis Tumor Models (Xenograft Models &amp; Syngeneic Models)</h4>
+                    </div>
+                    <div class="section-body">
+                        
+                    </div>
+                </div>
+            </section>
+            <section class="section-content mouse-models has-space">
+                <div class="container">
+                    <div class="section-header">
+                        <h4 class="section-title">Humanized Mouse Models</h4>
+                    </div>
+                    <div class="section-body">
+                    </div>
+                </div>
+            </section>
+            <section class="section-content patient-derived">
+                <div class="container">
+                    <div class="section-header">
+                        <h4 class="section-title">Patient-Derived Xonograft (PDX) Models</h4>
+                    </div>
+                    <div class="section-body">
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+			
+</main>
+
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+
+<script src="/assets/js/common.js"></script>
+
+</body>
+</html>
