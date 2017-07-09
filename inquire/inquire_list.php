e4dc64bc161550a66fd604bef2076b82fb6f00ad
kimyh5392 <mk.kimyh@gmail.com>
2017-07-09 19:55:48 +0900

    문의하기 > 리스트 및 뷰 완료

---
+++ inquire/inquire_list.php
@@ -0,0 +1,153 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link href="/assets/css/sub.css" rel="stylesheet">
+<link href="/assets/css/inquire.css" rel="stylesheet">
+<link href="/assets/css/form.css" rel="stylesheet">
+</head>
+<body class="inquire inquire-list">
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/top-nav.php'); ?>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/side-nav.php'); ?>
+<main id="content">
+    <div class="jumbo-wall">
+        <div class="container">
+            <h1>문의</h1>
+        </div>
+    </div>
+    
+    <div class="page-header">
+        <h1>상담문의</h1>
+    </div>
+    
+    <section class="section-content">
+        <div class="container">
+            <div class="section-header">
+                <h4 class="section-title">상담 문의 작성하기</h4>
+            </div>
+            <div class="section-body">
+                <div class="list">
+                    <ul class="dt-list">
+                        <li>번호</li>
+                        <li>제목</li>
+                        <li>작성일</li>
+                        <li>조회수</li>
+                    </ul>
+                    <div class="list-content">
+                        <ul class="dd-list">
+                            <li>
+                                <ul>
+                                    <li class="num">123</li>
+                                    <li class="subject">
+                                        <a href="#">공고합병 및 주권제출공고</a>
+                                    </li>
+                                    <li class="date">2017-04-01</li>
+                                    <li class="count">123</li>
+                                </ul>
+                            </li>
+                            <li>
+                                <ul>
+                                    <li class="num">123</li>
+                                    <li class="subject">
+                                        <a href="#">합병승인 임시주주총회 소집통지 공고</a>
+                                    </li>
+                                    <li class="date">2017-04-01</li>
+                                    <li class="count">123</li>
+                                </ul>
+                            </li>
+                            <li>
+                                <ul>
+                                    <li class="num">123</li>
+                                    <li class="subject">
+                                        <a href="#">주주명부폐쇄 기준일 공고</a>
+                                    </li>
+                                    <li class="date">2017-04-01</li>
+                                    <li class="count">123</li>
+                                </ul>
+                            </li>
+                            <li>
+                                <ul>
+                                    <li class="num">123</li>
+                                    <li class="subject">
+                                        <a href="#">결과분석에서 논문데이터까지 단 한번의 실험으로 해결하세요!</a>
+                                    </li>
+                                    <li class="date">2017-04-01</li>
+                                    <li class="count">123</li>
+                                </ul>
+                            </li>
+                            <li>
+                                <ul>
+                                    <li class="num">123</li>
+                                    <li class="subject">
+                                        <a href="#">CROWISE 효능/안전성 평가</a>
+                                    </li>
+                                    <li class="date">2017-04-01</li>
+                                    <li class="count">123</li>
+                                </ul>
+                            </li>
+                            <li>
+                                <ul>
+                                    <li class="num">123</li>
+                                    <li class="subject">
+                                        <a href="#">CROWISE 유전독성시험서비스(Genetic Toxicology)</a>
+                                    </li>
+                                    <li class="date">2017-04-01</li>
+                                    <li class="count">123</li>
+                                </ul>
+                            </li>
+                            <li>
+                                <ul>
+                                    <li class="num">123</li>
+                                    <li class="subject">
+                                        <a href="#">CROWISE 위탁사육관리</a>
+                                    </li>
+                                    <li class="date">2017-04-01</li>
+                                    <li class="count">123</li>
+                                </ul>
+                            </li>
+                            <li>
+                                <ul>
+                                    <li class="num">123</li>
+                                    <li class="subject">
+                                        <a href="#">CROWISE 비만효력시험</a>
+                                    </li>
+                                    <li class="date">2017-04-01</li>
+                                    <li class="count">123</li>
+                                </ul>
+                            </li>
+                            <li>
+                                <ul>
+                                    <li class="num">123</li>
+                                    <li class="subject">
+                                        <a href="#">CROWISE 병리평가</a>
+                                    </li>
+                                    <li class="date">2017-04-01</li>
+                                    <li class="count">123</li>
+                                </ul>
+                            </li>
+                            <li>
+                                <ul>
+                                    <li class="num">123</li>
+                                    <li class="subject">
+                                        <a href="#">CROWISE 당뇨 효력시험</a>
+                                    </li>
+                                    <li class="date">2017-04-01</li>
+                                    <li class="count">123</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="btn-area">
+                    <p>
+                        <a href="#" class="btn btn-red" role="button">등록하기</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+	
+</main>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+<script src="/assets/js/common.js"></script>
+</body>
+</html>
---
+++ inquire/inquire_view.php
@@ -0,0 +1,72 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link href="/assets/css/sub.css" rel="stylesheet">
+<link href="/assets/css/inquire.css" rel="stylesheet">
+<link href="/assets/css/form.css" rel="stylesheet">
+</head>
+<body class="inquire inquire-view">
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/top-nav.php'); ?>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/side-nav.php'); ?>
+<main id="content">
+    <div class="jumbo-wall">
+        <div class="container">
+            <h1>문의</h1>
+        </div>
+    </div>
+    
+    <div class="page-header">
+        <h1>상담문의</h1>
+    </div>
+    
+    <section class="section-content">
+        <div class="container">
+            <div class="section-header">
+                <h4 class="section-title">상담 문의 작성하기</h4>
+            </div>
+            <div class="section-body">
+                <div class="view">
+                    <h4 class="title">공고합병 및 주권제출공고</h4>
+                    <div class="content">
+                        <ul class="list-info">
+                            <li>2017-04-04</li>
+                            <li>
+                                <dl>
+                                    <dt>조회수</dt>
+                                    <dd>356</dd>
+                                </dl>
+                            </li>
+                        </ul>
+                        <div class="view-content">
+                            <p>
+                                한화엠지아이기업인수목적 주식회사(갑)는 2017년 3월 7일 개최한 임시 주주총회에서<br>
+                                주식회사 우정비에스씨(을)은 2017년 3월 7일 개최한 임시 주주총회에서 각각 합병을 결의하고 그 결과로<br>
+                                (갑)은 (을)을 합병하여 그 권리의무를 승계하고, (갑)은 존속하며 (을)은 해산하기로 결의하였으므로,<br>
+                                이 합병에 대하여 이의가 있는 채권자는 이 공고일 익일부터 1개월 이내에 관계회사에<br>
+                                이의를 제출하시고 주권을 가지고 있는 분은 관계회사에 주권을 제출하여 주시기 바라와 이에 공고 합니다.
+                            </p>
+                            <dl class="attach">
+                                <dt class="sr-only">첨부파일</dt>
+                                <dd>
+                                    <a href="#">20170510_181656_687.jpg</a>
+                                </dd>
+                            </dl>
+                        </div>
+                    </div>
+                </div>
+                <div class="btn-area">
+                    <p>
+                        <a href="#" class="btn btn-gray" role="button">삭제하기</a>
+                        <a href="#" class="btn btn-gray" role="button">수정하기</a>
+                        <a href="#" class="btn btn-red" role="button">목록으로</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+	
+</main>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+<script src="/assets/js/common.js"></script>
+</body>
+</html>
